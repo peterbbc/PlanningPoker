6ab13b310c9863b1983acfad5fc1a23505a2ce83
Peter Bloxidge <peter.bloxidge@bbc.co.uk>
2022-06-06 02:11:07 +0100

    Fix loader export to include camel AND kebab case exports

--- planning-poker-site/gatsby-config.ts
+++ planning-poker-site/gatsby-config.ts
@@ -2,17 +2,18 @@ import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `Planning Poker Site`,
-    siteUrl: `https://www.planningpokeronline.com`
+    title: "Planning Poker Site",
+    siteUrl: "https://www.planningpokeronline.com"
   },
   plugins: [
     {
-      resolve: `gatsby-plugin-sass`,
+      resolve: "gatsby-plugin-sass",
       options: {
         cssLoaderOptions: {
           esModule: false,
           modules: {
             namedExport: false,
+            exportLocalsConvention: "camelCase"
           },
         },
       },
